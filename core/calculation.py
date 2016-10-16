495501e604c6a837e7256cd350d67ee6af49cc7f
40323230 <daan0014119@gmail.com>
2016-10-16 17:08:25 +0800

    Python version change function

--- core/calculation.py
+++ core/calculation.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-from .slvs import *
+#SLVS Version
+import sys, platform
+py_nm = sys.version[0:sys.version.find(" ")][0:3]
 #IO
 from .io.slvs_type import *
 from .io.dxf_type import dxf_code
@@ -7,6 +9,13 @@ from .io.dxf_type import dxf_code
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+#SLVS Version
+if platform.system().lower()=="windows":
+    if py_nm=="3.5": from .kernel.py35w.slvs import *
+elif platform.system().lower()=="linux":
+    if py_nm=="3.5": from .kernel.py35.slvs import *
+    elif py_nm=="3.4": from .kernel.py34.slvs import *
+else: print("Python Version Not Support.")
 
 class Solvespace():
     def __init__(self):
