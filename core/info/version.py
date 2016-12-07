48a10960a96944a6e0874623471f6e92c94a673b
40323230 <daan0014119@gmail.com>
2016-12-07 19:34:10 +0800

    del Linux eric project cache

--- core/info/version.py
+++ core/info/version.py
@@ -12,10 +12,6 @@ class version_show(QDialog, Ui_About_Dialog):
         self.setupUi(self)
 
 def show_info():
-    commit = show_commit()
-    print("[Pyslvs "+version_number+"]")
-    if "-g" in argv or "--git" in argv:
-        print("Last Commit: "+commit)
     print("OS Type: "+platform.system())
     print("Python Version: {0:d}.{1:d}.{2:d}".format(*version_info[:3]))
     try:
@@ -47,7 +43,6 @@ Open a file directly by put file name behind the launch command.
 
 Information and Debug Function:
 
--g\t--git\t\tShow git infomations in the initialization.
 -v\t--version\tOnly show version infomations and not to start main program.
 -h\t--help\t\tShow this help message and not to start main program.
 
@@ -56,11 +51,3 @@ Run launch_test.py can start unit test.
 
 def show_version():
     print("[Pyslvs "+version_number+"]\nPython Version: {0:d}.{1:d}.{2:d}".format(*version_info[:3]))
-
-def show_commit():
-    data = []
-    with open('./.git/logs/refs/heads/master', newline="") as stream:
-        reader = csv.reader(stream, delimiter='\n', quotechar='|')
-        for row in reader: data += ', '.join(row).split(', ')
-    n = data[-1][41:81]
-    return n
