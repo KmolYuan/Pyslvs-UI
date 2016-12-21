12b072f7365e340f1b7110a45753983b6bba28ce
40323230 <daan0014119@gmail.com>
2016-12-22 01:25:15 +0800

    Add Windows build command.

---
+++ winBuild.bat
@@ -0,0 +1,15 @@
+@echo off
+
+echo ---Pyslvs Windows Build---
+
+rd .\build /s /q
+rd .\dist /s /q
+
+pyinstaller -w launch_pyslvs.py
+python setup.py build
+
+xcopy .\build\launch_pyslvs .\build\exe.win-amd64-3.5 /s /y
+
+echo ---Done---
+
+.\build\exe.win-amd64-3.5\launch_pyslvs.exe
