8707cc6c08cd5f2d3ab9ad56d13b274ed810cb18
40323230 <daan0014119@gmail.com>
2016-12-26 21:10:20 +0800

    UI and winBuild.bat fixed.

--- core/io/undoRedo.py
+++ core/io/undoRedo.py
@@ -3,7 +3,7 @@ from .modules import *
 from copy import deepcopy
 
 class addTableUndo(QUndoCommand):
-    def __init__(self, table, contentTable, list, contentList, row, edit):
+    def __init__(self, table, contentTable, list, contentList, row):
         QUndoCommand.__init__(self)
         self.table = table
         self.contentTable = deepcopy(contentTable) #List
@@ -42,13 +42,35 @@ class deleteTableUndo(QUndoCommand):
         self.list.pop(self.row)
         self.table.removeRow(self.row)
 
+class editTableUndo(QUndoCommand):
+    def __init__(self, table, contentTable1, contentTable2, list, contentList1, contentList2, row):
+        QUndoCommand.__init__(self)
+        self.table = table
+        self.contentTable1 = deepcopy(contentTable1) #List
+        self.contentTable2 = deepcopy(contentTable2) #List
+        self.list = list
+        self.contentList1 = deepcopy(contentList1) #List
+        self.contentList2 = deepcopy(contentList2) #List
+        self.row = row
+    
+    def undo(self):
+        self.list[self.row] = self.contentList
+        self.table.removeRow(self.row)
+    
+    def redo(self):
+        self.list.append(self.contentList)
+        for i in range(len(self.contentTable)):
+            name_set = QTableWidgetItem(self.contentTable[i])
+            if i==0: name_set.setFlags(Qt.ItemIsEnabled)
+            self.table.setItem(rowPosition, i, name_set)
+
 class moveTableUndo(QUndoCommand):
     def __init__(self):
-        QUndoCommand.__init__(self, table, list, preRow, nexRow)
+        QUndoCommand.__init__(self, table, list, row1, row2)
         self.table = table
         self.list = list
-        self.preRow = deepcopy(preRow)
-        self.nexRow = deepcopy(nexRow)
+        self.row1 = deepcopy(row1)
+        self.row2 = deepcopy(row2)
     
     def undo(self):
         ''''''
--- winBuild.bat
+++ winBuild.bat
@@ -5,10 +5,15 @@ echo ---Pyslvs Windows Build---
 rd .\build /s /q
 rd .\dist /s /q
 
-pyinstaller -w launch_pyslvs.py -i .\icons\main.ico
+pyinstaller -w launch_pyslvs.py -i .\icons\main_big.ico
 python setup.py build
 
+echo ---Copying Folder and Files---
+
 xcopy .\build\launch_pyslvs .\build\exe.win-amd64-3.5 /s /y
+rd .\dist /s /q
+rd .\build\launch_pyslvs /s /q
+del .\launch_pyslvs.spec
 
 echo ---Done---
 
