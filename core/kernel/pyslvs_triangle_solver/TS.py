bd5c951e56af0a7dbbc8596bd3d4cb962f4dbcdb
KmolYuan <daan0014119@gmail.com>
2017-03-13 00:33:11 +0800

    Fix functions (Not yet).

--- core/kernel/pyslvs_triangle_solver/TS.py
+++ core/kernel/pyslvs_triangle_solver/TS.py
@@ -38,31 +38,59 @@ class solver():
             elif e['Type']=='PLLP': results.append(self.PLLP(p1, e['len1'], e['len2'], p2))
         return results
     
-    def PLAP(self, p1, line1, angle, p2):
-        x1 = tuple(p1)[0]
-        y1 = tuple(p1)[1]
+    def PLAP(self, p1, line1, angle, p2, other=False):
+        x1 = p1[0]
+        y1 = p1[1]
         len1 = float(line1)
         angle2 = radians(float(angle))
         angle1 = self.m(p1, p2)
-        return x1+len1*cos(angle1+angle2), y1+len1*sin(angle1+angle2)
+        if other:
+            cx = x1+len1*cos(angle1-angle2)
+            cy = y1+len1*sin(angle1-angle2)
+        else:
+            cx = x1+len1*cos(angle1+angle2)
+            cy = y1+len1*sin(angle1+angle2)
+        return cx, cy
     
-    def PLLP(self, p1, line1, line2, p2):
-        x1 = tuple(p1)[0]
-        y1 = tuple(p1)[1]
+    def PLLP(self, p1, line1, line2, p2, other=False):
+        x1 = p1[0]
+        y1 = p1[1]
+        x2 = p2[0]
+        y2 = p2[1]
         len1 = float(line1)
         len2 = float(line2)
+        diffAB = sqrt((x1-x2)**2+(y2-y1)**2)
         angle1 = self.m(p1, p2)
-        angle2 = self.CosineTheoremAngle(len2, diffAB, len1)
-        return x1+len1*cos(angle1+angle2), y1+len1*sin(angle1+angle2)
+        angle2 = self.CosineTheoremAngle(len2, len1, diffAB)
+        print(degrees(angle2))
+        print(degrees(angle1))
+        if other:
+            cx = x1-len1*cos(angle1+angle2)
+            cy = y1+len1*sin(angle1+angle2)
+        else:
+            cx = x1+len1*cos(angle1-angle2)
+            cy = y1-len1*sin(angle1-angle2)
+        return cx, cy
     
     def m(self, p1, p2):
-        #Get info
-        x1 = tuple(p1)[0]
-        y1 = tuple(p1)[1]
-        x2 = tuple(p2)[0]
-        y2 = tuple(p2)[1]
-        #Solve
-        diffABx = x1-x2
-        diffABy = y1-y2
-        diffAB = sqrt(diffABx**2+diffABy**2)
-        return self.CosineTheoremAngle(diffABy, diffABx, diffAB)
+        x1 = p1[0]
+        y1 = p1[1]
+        x2 = p2[0]
+        y2 = p2[1]
+        x = abs(x2-x1)
+        y = abs(y2-y1)
+        diffAB = sqrt(x**2+y**2)
+        return self.CosineTheoremAngle(y, x, diffAB)
+
+if __name__=='__main__':
+    #Test
+    s = solver([
+        {'p1':(-60, 0), 'p2':(0, 0), 'len1':30, 'angle':50}, #C
+        {'p1':0, 'p2':(0, 0), 'len1':50, 'len2':60}, #D
+        {'p1':1, 'p2':0, 'len1':50, 'len2':50}, #E
+        ])
+    print(s.answer())
+    
+    ##cx= -40.716371709403816 cy= 22.98133329356934
+    ##dx= -6.698073034033397 dy= 59.62495968661744
+    ##ex= -55.44153371488418 ey= 70.76385733649067
