d973c6eeb677a0eeae2d8efbb93ebfa54d9a218e
KmolYuan <daan0014119@gmail.com>
2017-03-16 19:31:08 +0800

    Fix Rod panel slider bar.

--- core/io/example.py
+++ core/io/example.py
@@ -170,7 +170,7 @@ def example_liftTailgate():
         '_info_', 'Yuang', '_info_', 'Example: Lift Tailgate', '_info_', timeNow, '_info_',
         '_table_', 'Point1', '50.0', '0.0', 'True', 'Point2', '25.53', '-46.05', 'False', 'Point3', '35.26', '-57.11', 'False', 'Point4', '30.0', '-24.21', 'False', 'Point5', '66.58', '-40.0', 'False', 'Point6', '67.63', '-20.0', 'True', 'Point7', '161.84', '-1.05', 'False',
         '_table_', 'Point1', 'Blue', '10', 'Blue', 'Point2', 'Green', '5', 'Green', 'Point3', 'Green', '5', 'Green', 'Point4', 'Green', '5', 'Green', 'Point5', 'Green', '5', 'Green', 'Point6', 'Blue', '10', 'Blue', 'Point7', 'Dark-Yellow', '5', 'Dark-Yellow',
-        '_table_', 'Line0', 'Point1', 'Point2', '50.0', 'Line1', 'Point0', 'Point3', '80.0', 'Line2', 'Point4', 'Point5', '30.0',
+        '_table_', 'Line0', 'Point1', 'Point2', '50.0', 'Line1', 'Point0', 'Point3', '77.0', 'Line2', 'Point4', 'Point5', '30.0',
         '_table_', 'Chain0', 'Point6', 'Point5', 'Point7', '20.0', '150.0', '140.0',
         '_table_',
         '_table_',
--- core/panel/run_Drive_rod.py
+++ core/panel/run_Drive_rod.py
@@ -15,7 +15,7 @@ class Drive_rod_show(QWidget, Ui_Form):
     def on_Rod_currentIndexChanged(self, index):
         start = self.table[index]['start']
         end = self.table[index]['end']
-        distance = int(((self.tablePoint[start]['x']-self.tablePoint[end]['x'])**2+(self.tablePoint[start]['y']-self.tablePoint[end]['y'])**2)**(1/2)*100)
+        distance = int(((self.tablePoint[start]['cx']-self.tablePoint[end]['cx'])**2+(self.tablePoint[start]['cy']-self.tablePoint[end]['cy'])**2)**(1/2)*100)
         self.Position.setMaximum(distance)
         self.Position.setValue(int(self.table[index]['pos']*100))
         self.Distance_text.setValue(distance/100)
