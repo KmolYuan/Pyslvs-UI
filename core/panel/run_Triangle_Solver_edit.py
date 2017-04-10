8566efeb437ccc405b4df6b71cdea7803be1dfbd
KmolYuan <daan0014119@gmail.com>
2017-04-10 21:13:05 +0800

    TS editor defensive measures.

--- core/panel/run_Triangle_Solver_edit.py
+++ core/panel/run_Triangle_Solver_edit.py
@@ -10,7 +10,6 @@ class Triangle_Solver_edit_show(QDialog, Ui_Dialog):
             for e in [self.p1, self.p2, self.p3]: e.addItem(QIcon(QPixmap(":/icons/point.png")), 'Point{}'.format(i))
         for i in range(row):
             for e in [self.r1, self.r2, self.r3]: e.addItem(QIcon(QPixmap(":/icons/TS.png")), 'Result{}'.format(i+1))
-        self.buttonBox.button(QDialogButtonBox.Ok).clicked.connect(self.turn2Dict)
         self.p1Result.setEnabled(row>0)
         self.p2Result.setEnabled(row>0)
         self.p3Result.setEnabled(row>0)
@@ -54,6 +53,7 @@ class Triangle_Solver_edit_show(QDialog, Ui_Dialog):
                 elif type(condition['p3'])==str:
                     self.p3Exist.setChecked(True)
                     self.p3.setCurrentIndex(int(condition['p3'].replace('Point', '')))
+        self.isOk()
     
     @pyqtSlot(int)
     def on_type_currentIndexChanged(self, pos):
@@ -71,6 +71,7 @@ class Triangle_Solver_edit_show(QDialog, Ui_Dialog):
         if pos==2: self.merge.insertItems(0, ["Points only", "Slider"])
         else: self.merge.insertItems(0, ["Points only", "Linking L0", "Linking R0", "Stay Chain", "Linking L0 & R0"])
         self.merge.setCurrentIndex(0)
+        self.isOk()
     
     @pyqtSlot(int)
     def on_merge_currentIndexChanged(self, pos):
@@ -86,26 +87,87 @@ class Triangle_Solver_edit_show(QDialog, Ui_Dialog):
                 elif pos==1: pic = ":/icons/preview/TSMergeSlider.png"
             self.mergeImage.setPixmap(QPixmap(pic).scaledToWidth(560))
     
-    def turn2Dict(self):
+    def isOk(self):
         self.condition = {
             'Type':self.type.currentText(),
             'p1':(self.x1.value(), self.y1.value()) if self.p1Customize.isChecked() else
                 self.p1.currentText() if self.p1Exist.isChecked() else self.r1.currentIndex(),
             'p2':(self.x2.value(), self.y2.value()) if self.p2Customize.isChecked() else
                 self.p2.currentText() if self.p2Exist.isChecked() else self.r2.currentIndex(),
-            'merge':self.merge.currentIndex(),
-        }
-        if self.type.currentIndex()==0: triangle = {
-            'len1':self.len1.value(),
-            'angle':self.angle.value() if self.R1Exist.isChecked() else False}
-        elif self.type.currentIndex()==1: triangle = {
-            'len1':self.len1.value(),
-            'len2':self.len2.value() if self.R1Exist.isChecked() else False}
-        elif self.type.currentIndex()==2: triangle = {
-            'len1':self.len1.value(),
-            'p3':(self.x3.value(), self.y3.value()) if self.p3Customize.isChecked() else
-            self.p3.currentText() if self.p3Exist.isChecked() else self.r3.currentIndex()}
-        elif self.type.currentIndex()==3: triangle = {
-            'p3':(self.x3.value(), self.y3.value()) if self.p3Customize.isChecked() else
-            self.p3.currentText() if self.p3Exist.isChecked() else self.r3.currentIndex()}
+            'merge':self.merge.currentIndex()}
+        n = self.condition['p1']!=self.condition['p2']
+        if self.type.currentIndex()==0:
+            triangle = {
+                'len1':self.len1.value(),
+                'angle':self.angle.value(),
+                'other':self.other.isChecked()}
+            n &= triangle['len1']>=0
+        elif self.type.currentIndex()==1:
+            triangle = {
+                'len1':self.len1.value(),
+                'len2':self.len2.value(),
+                'other':self.other.isChecked()}
+            n &= triangle['len1']>=0 and triangle['len2']>=0
+        elif self.type.currentIndex()==2:
+            triangle = {
+                'len1':self.len1.value(),
+                'p3':(self.x3.value(), self.y3.value()) if self.p3Customize.isChecked() else
+                self.p3.currentText() if self.p3Exist.isChecked() else self.r3.currentIndex(),
+                'other':self.other.isChecked()}
+            n &= triangle['len1']>=0 and triangle['p3']!=self.condition['p1'] and triangle['p3']!=self.condition['p2']
+        elif self.type.currentIndex()==3:
+            triangle = {
+                'p3':(self.x3.value(), self.y3.value()) if self.p3Customize.isChecked() else
+                self.p3.currentText() if self.p3Exist.isChecked() else self.r3.currentIndex()}
+            n &= triangle['p3']!=self.condition['p1'] and triangle['p3']!=self.condition['p2']
         self.condition.update(triangle)
+        self.buttonBox.button(QDialogButtonBox.Ok).setEnabled(n)
+    
+    @pyqtSlot()
+    def on_p1Exist_clicked(self): self.isOk()
+    @pyqtSlot(int)
+    def on_p1_currentIndexChanged(self, index): self.isOk()
+    @pyqtSlot()
+    def on_p1Customize_clicked(self): self.isOk()
+    @pyqtSlot(float)
+    def on_x1_valueChanged(self, p0): self.isOk()
+    @pyqtSlot(float)
+    def on_y1_valueChanged(self, p0): self.isOk()
+    @pyqtSlot()
+    def on_p1Result_clicked(self): self.isOk()
+    @pyqtSlot(int)
+    def on_r1_currentIndexChanged(self, index): self.isOk()
+    @pyqtSlot()
+    def on_p2Exist_clicked(self): self.isOk()
+    @pyqtSlot(int)
+    def on_p2_currentIndexChanged(self, index): self.isOk()
+    @pyqtSlot()
+    def on_p2Customize_clicked(self): self.isOk()
+    @pyqtSlot(float)
+    def on_x2_valueChanged(self, p0): self.isOk()
+    @pyqtSlot(float)
+    def on_y2_valueChanged(self, p0): self.isOk()
+    @pyqtSlot()
+    def on_p2Result_clicked(self): self.isOk()
+    @pyqtSlot(int)
+    def on_r2_currentIndexChanged(self, index): self.isOk()
+    @pyqtSlot()
+    def on_p3Exist_clicked(self): self.isOk()
+    @pyqtSlot(int)
+    def on_p3_currentIndexChanged(self, index): self.isOk()
+    @pyqtSlot()
+    def on_p3Customize_clicked(self): self.isOk()
+    @pyqtSlot(float)
+    def on_x3_valueChanged(self, p0): self.isOk()
+    @pyqtSlot(float)
+    def on_y3_valueChanged(self, p0): self.isOk()
+    @pyqtSlot()
+    def on_p3Result_clicked(self): self.isOk()
+    @pyqtSlot(int)
+    def on_r3_currentIndexChanged(self, index): self.isOk()
+    @pyqtSlot(float)
+    def on_len1_valueChanged(self, p0): self.isOk()
+    @pyqtSlot(float)
+    def on_len2_valueChanged(self, p0): self.isOk()
+    @pyqtSlot(float)
+    def on_angle_valueChanged(self, p0): self.isOk()
