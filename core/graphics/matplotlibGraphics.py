ca7a5b19d3b40461cd05a1373ab1be437ce05c7d
KmolYuan <daan0014119@gmail.com>
2017-04-27 17:34:26 +0800

    Remove y lable.

--- core/graphics/matplotlibGraphics.py
+++ core/graphics/matplotlibGraphics.py
@@ -19,7 +19,6 @@ class BasicChart(FigureCanvas):
     def initial_figure(self):
         x = [i for i in range(len(self.data))]
         y = self.data
-        self.axes.xlabel("Progress Rate")
         self.axes.plot(x, y, 'g')
 
 class BasicChartDialog(QDialog):
