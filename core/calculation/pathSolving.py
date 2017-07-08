2823f7351b258fcad00ad1fda04c1cc53a6ace1f
KmolYuan <daan0014119@gmail.com>
2017-07-08 17:11:29 +0800

    Moved algorithm Cython function into QThread, so we could terminate it.

--- core/calculation/pathSolving.py
+++ core/calculation/pathSolving.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from ..QtModules import *
-from .algorithm import generateProcess
-import timeit
+import timeit, numpy
+from ..kernel.kernel_getter import build_planar, Genetic, Firefly, DiffertialEvolution
 
 class WorkerThread(QThread):
     done = pyqtSignal(dict, int)
@@ -20,7 +20,7 @@ class WorkerThread(QThread):
         print("Algorithm: "+alg)
         print("Through: {}".format(self.mechanismParams['targetPath']))
         t0 = timeit.default_timer()
-        TnF, FP = generateProcess(self.type_num, self.mechanismParams, self.GenerateData, self.algorithmPrams)
+        TnF, FP = self.generateProcess(self.type_num, self.mechanismParams, self.GenerateData, self.algorithmPrams)
         if self.stoped: return
         t1 = timeit.default_timer()
         time_spand = t1-t0
@@ -36,6 +36,53 @@ class WorkerThread(QThread):
             'TimeAndFitness':TnF}
         print('total cost time: {:.4f} [s]'.format(time_spand))
         self.done.emit(mechanism, time_spand)
+    
+    def generateProcess(self, type_num, mechanismParams, GenerateData, algorithmPrams):
+        mechanismObj = build_planar(mechanismParams)
+        #Genetic Algorithm
+        if type_num==0:
+            APs = {
+                'nParm':GenerateData['nParm'],
+                'nPop':algorithmPrams['nPop'], #250
+                'pCross':algorithmPrams['pCross'], #0.95
+                'pMute':algorithmPrams['pMute'], #0.05
+                'pWin':algorithmPrams['pWin'], #0.95
+                'bDelta':algorithmPrams['bDelta'], #5.
+                'upper':GenerateData['upper'],
+                'lower':GenerateData['lower'],
+                'maxGen':GenerateData['maxGen'],
+                'report':GenerateData['report']}
+            self.foo = Genetic(mechanismObj, **APs)
+        #Firefly Algorithm
+        elif type_num==1:
+            APs = {
+                'D':GenerateData['nParm'],
+                'n':algorithmPrams['n'], #40
+                'alpha':algorithmPrams['alpha'], #0.01
+                'betaMin':algorithmPrams['betaMin'], #0.2
+                'gamma':algorithmPrams['gamma'], #1.
+                'beta0':algorithmPrams['beta0'], #1.
+                'ub':GenerateData['upper'],
+                'lb':GenerateData['lower'],
+                'maxGen':GenerateData['maxGen'],
+                'report':GenerateData['report']}
+            self.foo = Firefly(mechanismObj, **APs)
+        #Differential Evolution
+        elif type_num==2:
+            APs = {
+                'D':GenerateData['nParm'],
+                'strategy':algorithmPrams['strategy'], #1
+                'NP':algorithmPrams['NP'], #190
+                'F':algorithmPrams['F'], #0.6
+                'CR':algorithmPrams['CR'], #0.9
+                'upper':GenerateData['upper'],
+                'lower':GenerateData['lower'],
+                'maxGen':GenerateData['maxGen'],
+                'report':GenerateData['report']}
+            self.foo = DiffertialEvolution(mechanismObj, **APs)
+        time_and_fitness, fitnessParameter = self.foo.run()
+        return([float(k[1]) for k in [e.split(',') for e in time_and_fitness.split(';')[0:-1]]],
+            [float(e) for e in fitnessParameter.split(',')])
     
     def stop(self):
         with QMutexLocker(self.mutex): self.stoped = True
