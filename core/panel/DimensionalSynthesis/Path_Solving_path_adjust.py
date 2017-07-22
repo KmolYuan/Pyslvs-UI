2c29c80c523e22711d8a14e66731c52f907995dc
KmolYuan <daan0014119@gmail.com>
2017-07-22 16:53:33 +0800

    Add blurring function.

--- core/panel/DimensionalSynthesis/Path_Solving_path_adjust.py
+++ core/panel/DimensionalSynthesis/Path_Solving_path_adjust.py
@@ -25,17 +25,33 @@ class Path_Solving_path_adjust_show(QDialog, Ui_Dialog):
         super(Path_Solving_path_adjust_show, self).__init__(parent)
         self.setupUi(self)
         for e in path: self.Point_list.addItem("({}, {})".format(e['x'], e['y']))
+        self.points_num.setText(str(len(path)))
+        self.blurring_num.setMaximum(len(path))
+        self.blurring_num.setValue(self.blurring_num.maximum())
         self.path = path
         self.r_path = list()
     
     def get_path(self): return self.r_path
     
     @pyqtSlot()
-    def on_scalingButton_clicked(self): self.r_path = [
-        (self.scaling_rx.value()+(e['x']-self.scaling_rx.value())*self.scaling_h.value(), self.scaling_ry.value()+(e['y']-self.scaling_ry.value())*self.scaling_v.value())
-        for e in self.path]
+    def on_scalingButton_clicked(self):
+        self.r_path = [
+            (self.scaling_rx.value()+(e['x']-self.scaling_rx.value())*self.scaling_h.value(),
+            self.scaling_ry.value()+(e['y']-self.scaling_ry.value())*self.scaling_v.value()) for e in self.path]
+        self.accept()
     
     @pyqtSlot()
-    def on_movingButton_clicked(self): self.r_path = [
-        (e['x']+self.moving_x_coordinate.value(), e['y']+self.moving_y_coordinate.value())
-        for e in self.path]
+    def on_movingButton_clicked(self):
+        self.r_path = [(e['x']+self.moving_x_coordinate.value(), e['y']+self.moving_y_coordinate.value()) for e in self.path]
+        self.accept()
+    
+    @pyqtSlot()
+    def on_blurringButton_clicked(self):
+        try: target_num = round(len(self.path)/(len(self.path)-self.blurring_num.value()))
+        except ZeroDivisionError: target_num = len(self.path)
+        self.r_path = [(e['x'], e['y']) for i, e in enumerate(self.path) if i%target_num!=0]
+        f_p = (self.path[0]['x'], self.path[0]['y'])
+        e_p = (self.path[-1]['x'], self.path[-1]['y'])
+        if self.r_path[0]!=f_p: self.r_path.insert(0, f_p)
+        if self.r_path[-1]!=e_p: self.r_path.append(e_p)
+        self.accept()
