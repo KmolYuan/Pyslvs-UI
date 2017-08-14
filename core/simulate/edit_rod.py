2d6e7a1b30b0414d5073ae497b9957424f77c477
KmolYuan <daan0014119@gmail.com>
2017-08-14 14:22:20 +0800

    Fix Rod edit bug.

--- core/simulate/edit_rod.py
+++ core/simulate/edit_rod.py
@@ -40,7 +40,7 @@ class edit_rod_show(QDialog, edit_rod_Dialog):
             for i in range(len(Rods)):
                 self.Rod.insertItem(i, iconSelf, 'Rod{}'.format(i))
             self.Rod.setCurrentIndex(pos)
-        for signal in [self.Center.currentIndexChanged, self.on_Start.currentIndexChanged, self.End.currentIndexChanged,
+        for signal in [self.Center.currentIndexChanged, self.Start.currentIndexChanged, self.End.currentIndexChanged,
                 self.Position.valueChanged, self.Position.editingFinished]:
             signal.connect(self.isOk)
         self.isOk()
