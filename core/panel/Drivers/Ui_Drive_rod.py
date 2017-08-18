cffc172499b7d89f422bc714391f44e5fb2e7b7a
40323230 <daan0014119@gmail.com>
2017-08-18 14:51:16 +0800

    UI improvements.

--- core/panel/Drivers/Ui_Drive_rod.py
+++ core/panel/Drivers/Ui_Drive_rod.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/home/ahshoe/Desktop/Pyslvs-PyQt5/core/panel/Drivers/Drive_rod.ui'
+# Form implementation generated from reading ui file 'C:\Users\ahshoe\Desktop\Pyslvs-PyQt5\core\panel\Drivers\Drive_rod.ui'
 #
 # Created by: PyQt5 UI code generator 5.9
 #
--- core/simulate/Ui_edit_shaft.py
+++ core/simulate/Ui_edit_shaft.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/home/ahshoe/Desktop/Pyslvs-PyQt5/core/simulate/edit_shaft.ui'
+# Form implementation generated from reading ui file 'C:\Users\ahshoe\Desktop\Pyslvs-PyQt5\core\simulate\edit_shaft.ui'
 #
 # Created by: PyQt5 UI code generator 5.9
 #
