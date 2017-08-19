93d226b79b890e2ef6892fed0281b02261d70bf4
KmolYuan <daan0014119@gmail.com>
2017-08-19 18:02:13 +0800

    Add elements list.

--- core/io/elements.py
+++ core/io/elements.py
@@ -17,69 +17,83 @@
 ##along with this program; if not, write to the Free Software
 ##Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
+class VList:
+    def __init__(self):
+        self.__list = []
+    
+    def __getitem__(self, i):
+        return self.__list[i]
+    
+    def pop(self):
+        return self.__list.pop()
+
 class VPoint:
     def __init__(self, x=0., y=0., fix=False, color='Red'):
         self.set(x, y, fix)
         self.setColor(color)
-        self._cx = self._x
-        self._cy = self._y
+        self.__cx = self.__x
+        self.__cy = self.__y
     
     @property
     def x(self):
-        return self._x
+        return self.__x
     @property
     def y(self):
-        return self._y
+        return self.__y
     @x.setter
     def x(self, x):
-        self._x = x
+        self.__x = x
     @y.setter
     def y(self, y):
-        self._y = y
+        self.__y = y
     @property
     def fix(self):
-        return self._fix
+        return self.__fix
     @property
     def color(self):
-        return self._color
+        return self.__color
     @property
     def cx(self):
-        return self._cx
+        return self.__cx
     @property
     def cy(self):
-        return self._cy
+        return self.__cy
     
-    def set(self, x=0., y=0., fix=False):
-        self._x = x
-        self._y = y
-        self._fix = fix
+    def set(self, x, y, fix):
+        self.__x = x
+        self.__y = y
+        self.__fix = fix
     
     def round(self, d=8):
-        self._x = round(self._x, d)
-        self._y = round(self._y, d)
+        self.__x = round(self.__x, d)
+        self.__y = round(self.__y, d)
     
     def setColor(self, color='Red'):
-        self._color = color
+        self.__color = color
     
     def move(self, x=None, y=None):
         if x==None:
-            x = self._x
+            x = self.__x
         if y==None:
-            y = self._y
-        self._cx = x
-        self._cy = y
+            y = self.__y
+        self.__cx = x
+        self.__cy = y
     
     def reset(self):
-        self._x = self._cx
-        self._y = self._cy
+        self.__x = self.__cx
+        self.__y = self.__cy
     
     def items(self, index=0):
         return ('Point{}'.format(index), self.x, self.y, self.fix, self.color)
     def items_tags(self, index=0):
         return ('Point{}'.format(index), ('x', self.x), ('y', self.y), ('fix', self.fix), ('color', self.color))
+
+class VPointList(VList):
+    def __init__(self):
+        self.__list = [VPoint(fix=True)]
     
-    def __str__(self):
-        return "<Point x={v.x} y={v.y} fix={v.fix} cx={v.cx} cy={v.cy}>".format(v=self)
+    def add(self, x, y, fix, color):
+        self.__list.append(VPoint(x, y, fix, color))
 
 class VLine:
     def __init__(self, start=0, end=0, len=0.):
@@ -87,18 +101,18 @@ class VLine:
     
     @property
     def start(self):
-        return self._start
+        return self.__start
     @property
     def end(self):
-        return self._end
+        return self.__end
     @property
     def len(self):
-        return self._len
+        return self.__len
     
-    def set(self, start=VPoint(), end=VPoint(), len=0.):
-        self._start = start
-        self._end = end
-        self._len = len
+    def set(self, start, end, len):
+        self.__start = start
+        self.__end = end
+        self.__len = len
     
     def items(self, index=0):
         return ('Line{}'.format(index), self.start, self.end, self.len)
@@ -106,9 +120,11 @@ class VLine:
         return ('Line{}'.format(index), ('start', self.start), ('end', self.end), ('len', self.len))
     
     def __contains__(self, point):
-        return point==self._start or point==self._end
-    def __str__(self):
-        return "<Line start={v.start} end={v.end} len={v.len}>".format(v=self)
+        return point==self.start or point==self.end
+
+class VLineList(VList):
+    def add(self, start, end, len):
+        self.__list.append(VLine(start, end, len))
 
 class VChain:
     def __init__(self, p1=VPoint(), p2=0, p3=0, p1p2=0., p2p3=0., p1p3=0.):
@@ -116,30 +132,30 @@ class VChain:
     
     @property
     def p1(self):
-        return self._p1
+        return self.__p1
     @property
     def p2(self):
-        return self._p2
+        return self.__p2
     @property
     def p3(self):
-        return self._p3
+        return self.__p3
     @property
     def p1p2(self):
-        return self._p1p2
+        return self.__p1p2
     @property
     def p2p3(self):
-        return self._p2p3
+        return self.__p2p3
     @property
     def p1p3(self):
-        return self._p1p3
+        return self.__p1p3
     
-    def set(self, p1=0, p2=0, p3=0, p1p2=0., p2p3=0., p1p3=0.):
-        self._p1 = p1
-        self._p2 = p2
-        self._p3 = p3
-        self._p1p2 = p1p2
-        self._p2p3 = p2p3
-        self._p1p3 = p1p3
+    def set(self, p1, p2, p3, p1p2, p2p3, p1p3):
+        self.__p1 = p1
+        self.__p2 = p2
+        self.__p3 = p3
+        self.__p1p2 = p1p2
+        self.__p2p3 = p2p3
+        self.__p1p3 = p1p3
     
     def items(self, index=0):
         return ('Chain{}'.format(index), self.p1, self.p2, self.p3, self.p1p2, self.p2p3, self.p1p3)
@@ -147,9 +163,11 @@ class VChain:
         return ('Chain{}'.format(index), ('p1', self.p1), ('p2', self.p2), ('p3', self.p3), ('p1p2', self.p1p2), ('p2p3', self.p2p3), ('p1p3', self.p1p3))
     
     def __contains__(self, point):
-        return point==self._p1 or point==self._p2 or point==self._p3
-    def __str__(self):
-        return "<Chain p1={v.p1} p2={v.p2} p3={v.p3} p1p2={v.p1p2} p2p3={v.p2p3} p1p3={v.p1p3}>".format(v=self)
+        return point==self.p1 or point==self.p2 or point==self.p3
+
+class VChainList(VList):
+    def add(self, p1, p2, p3, p1p2, p2p3, p1p3):
+        self.__list.append(VChain(p1, p2, p3, p1p2, p2p3, p1p3))
 
 class VShaft:
     def __init__(self, cen=0, ref=0, start=0., end=360., demo=0.):
@@ -157,33 +175,33 @@ class VShaft:
     
     @property
     def cen(self):
-        return self._cen
+        return self.__cen
     @property
     def ref(self):
-        return self._ref
+        return self.__ref
     @property
     def start(self):
-        return self._start
+        return self.__start
     @property
     def end(self):
-        return self._end
+        return self.__end
     @property
     def demo(self):
-        return self._demo
+        return self.__demo
     @demo.setter
     def demo(self, demo):
-        self._demo = demo
+        self.__demo = demo
     
-    def set(self, cen=0, ref=0, start=0., end=360., demo=0.):
-        self._cen = cen
-        self._ref = ref
-        self._start = start
-        self._end = end
-        self._demo = demo
+    def set(self, cen, ref, start, end, demo):
+        self.__cen = cen
+        self.__ref = ref
+        self.__start = start
+        self.__end = end
+        self.__demo = demo
     
     def drive(self, demo):
-        if demo>self._start and demo<self._end:
-            self._demo = demo
+        if demo>self.start and demo<self.end:
+            self.__demo = demo
     
     def items(self, index=0):
         return ('Shaft{}'.format(index), self.cen, self.ref, self.start, self.end, self.demo)
@@ -191,9 +209,11 @@ class VShaft:
         return ('Shaft{}'.format(index), ('cen', self.cen), ('ref', self.ref), ('start', self.start), ('end', self.end), ('demo', self.demo))
     
     def __contains__(self, point):
-        return point==self._cen or point==self._ref
-    def __str__(self):
-        return "<Shaft cen={v.cen} ref={v.ref} start={v.start}, end={v.end} demo={v.demo}".format(v=self)
+        return point==self.cen or point==self.ref
+
+class VShaftList(VList):
+    def add(self, cen, ref, start, end, demo):
+        self.__list.append(VShaft(cen, ref, start, end, demo))
 
 class VSlider:
     def __init__(self, cen=0, start=0, end=0):
@@ -201,18 +221,18 @@ class VSlider:
     
     @property
     def cen(self):
-        return self._cen
+        return self.__cen
     @property
     def start(self):
-        return self._start
+        return self.__start
     @property
     def end(self):
-        return self._end
+        return self.__end
     
-    def set(self, cen=0, start=0, end=0):
-        self._cen = cen
-        self._start = start
-        self._end = end
+    def set(self, cen, start, end):
+        self.__cen = cen
+        self.__start = start
+        self.__end = end
     
     def items(self, index=0):
         return ('Slider{}'.format(index), self.cen, self.start, self.end)
@@ -220,9 +240,11 @@ class VSlider:
         return ('Slider{}'.format(index), ('cen', self.cen), ('start', self.start), ('end', self.end))
     
     def __contains__(self, point):
-        return point==self._cen or point==self._start or point==self._end
-    def __str__(self):
-        return "<Slider cen={v.cen} start={v.start} end={v.end}>".format(v=self)
+        return point==self.cen or point==self.start or point==self.end
+
+class VSliderList(VList):
+    def add(self, cen, start, end):
+        self.__list.append(VSlider(cen, start, end))
 
 class VRod(VSlider):
     def __init__(self, cen=0, start=0, end=0, pos=0.):
@@ -233,41 +255,43 @@ class VRod(VSlider):
         return self._pos
     @pos.setter
     def pos(self, pos):
-        self._pos = pos
+        self.__pos = pos
     
     def set(self, cen=0, start=0, end=0, pos=0.):
         super(VRod, self).set(cen, start, end)
-        self._pos = pos
+        self.__pos = pos
     
     def items(self, index=0):
         return ('Rod{}'.format(index), self.cen, self.start, self.end, self.pos)
     def items_tags(self, index=0):
         return ('Rod{}'.format(index), ('cen', self.cen), ('start', self.start), ('end', self.end), ('pos', self.pos))
-    
-    def __str__(self):
-        return "<Rod cen={v.cen} start={v.start} end={v.end} pos={v.pos}>".format(v=self)
+
+class VRodList(VList):
+    def add(self, cen, start, end, pos):
+        self.__list.append(VRod(cen, start, end, pos))
 
 class VParameter:
     def __init__(self, val=0., commit=''):
         self.set(val, commit)
     @property
     def val(self):
-        return self._val
+        return self.__val
     @property
     def commit(self):
         return self._commit
     
     def set(self, val=0., commit=''):
-        self._val = val
-        self._commit = commit
+        self.__val = val
+        self.__commit = commit
     
     def items(self, index=0):
         return ('n{}'.format(index), self.val, self.commit)
     def items_tags(self, index=0):
         return ('n{}'.format(index), ('val', self.val), ('commit', self.commit))
-    
-    def __str__(self):
-        return "<Parameter val={v.val} commit=\"{v.commit}\">".format(v=self)
+
+class VParameterList(VList):
+    def add(self, val, commit):
+        self.__list.append(VParameter(val, commit))
 
 class VPath:
     def __init__(self, point=0, points=list(), show=True):
@@ -275,67 +299,62 @@ class VPath:
     
     @property
     def point(self):
-        return self._point
+        return self.__point
     @property
     def path(self):
-        return self._path
+        return self.__path
     @property
     def show(self):
-        return self._show
+        return self.__show
     @show.setter
     def show(self, show):
-        self._show = show
+        self.__show = show
     
     def set(self, point=0, points=list(), show=True):
-        self._point = point
-        self._path = list()
-        self._show = show
+        self.__point = point
+        self.__path = list()
+        self.__show = show
         if points:
             for p in points:
                 PointType = type(p)
                 if PointType==tuple or PointType==list or p==None:
                     #(x, y)
-                    self._path.append(p)
+                    self.__path.append(p)
     
     def isBroken(self):
         for point in self.path:
             if point is False or point[0] is False:
                 return True
         return False
-    
-    def __str__(self):
-        return "<Path point={v.point} path={v.path}>".format(v=self)
 
 class VPaths:
     def __init__(self, shaft=0, paths=list()):
         self.set(shaft, paths)
     @property
     def shaft(self):
-        return self._shaft
+        return self.__shaft
     @property
     def paths(self):
-        return self._paths
+        return self.__paths
     
     def set(self, shaft=0, paths=list()):
-        self._shaft = shaft
-        self._paths = list()
+        self.__shaft = shaft
+        self.__paths = list()
         if paths:
             for path in paths:
                 if type(path)==VPath:
-                    self._paths.append(path)
+                    self.__paths.append(path)
     
     def isBroken(self):
         for path in self.paths:
             if path.isBroken():
                 return True
         return False
-    
-    def __str__(self):
-        return "<Paths shaft={v.shaft} paths={v.paths}>".format(v=self)
+
+class VPathsList(VList):
+    def add(self, shaft, paths):
+        self.__list.append(VPaths(shaft, paths))
 
 if __name__=='__main__':
-    a = VPath(1, (None, (0, 2)))
-    b = VPath(2, ((3, 5), (0, 2)))
-    print(a.path)
-    c = VPaths(0, (a, b))
-    print(c.paths)
+    a = VPoint(10, 20)
+    print(a)
