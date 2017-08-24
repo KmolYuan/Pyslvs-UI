5b48ae3e4b8e2f07384e3e3b03b4d9d8bf7df726
40323230 <daan0014119@gmail.com>
2017-08-24 14:55:12 +0800

    Sort out setup.py code.

--- core/kernel/pyslvs_algorithm/setup.py
+++ core/kernel/pyslvs_algorithm/setup.py
@@ -2,13 +2,6 @@ from distutils.core import setup, Extension
 from Cython.Build import cythonize
 import numpy
 
-ext_modules = [
-    Extension("demo",
-        sources=["rga.pyx"],
-        libraries=["m"] # Unix-like specific
-    )
-]
-
 setup(
     ext_modules=cythonize('*.pyx', compiler_directives={'boundscheck':True}),
     include_dirs=[numpy.get_include()]
