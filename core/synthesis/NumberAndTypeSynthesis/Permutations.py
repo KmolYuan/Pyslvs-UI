7794ad760c8880b2d13dcf9cbfd3b3283c403579
KmolYuan <daan0014119@gmail.com>
2017-10-23 22:06:46 +0800

    Number synthesis function, will be shown as 'Distant relatives' attributes.

--- core/synthesis/NumberAndTypeSynthesis/Permutations.py
+++ core/synthesis/NumberAndTypeSynthesis/Permutations.py
@@ -1,7 +1,55 @@
 # -*- coding: utf-8 -*-
+##Pyslvs - Open Source Planar Linkage Mechanism Simulation and Dimensional Synthesis System.
+##Copyright (C) 2016-2017 Yuan Chang
+##E-mail: pyslvs@gmail.com
+##
+##This program is free software; you can redistribute it and/or modify
+##it under the terms of the GNU Affero General Public License as published by
+##the Free Software Foundation; either version 3 of the License, or
+##(at your option) any later version.
+##
+##This program is distributed in the hope that it will be useful,
+##but WITHOUT ANY WARRANTY; without even the implied warranty of
+##MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##GNU Affero General Public License for more details.
+##
+##You should have received a copy of the GNU Affero General Public License
+##along with this program; if not, write to the Free Software
+##Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 from ...QtModules import *
 from .Ui_Permutations import Ui_Form
+from itertools import product
+
+class NumberSynthesis:
+    def __init__(self, NL, NJ):
+        self.NL = NL
+        self.NJ = NJ
+    
+    @property
+    def Mmax(self):
+        if self.NL <= self.NJ and self.NJ <= (2*self.NL-3):
+            return self.NJ - self.NL + 2
+        elif (2*self.NL-3) <= self.NJ and self.NJ <= (self.NL*(self.NL-1)/2):
+            return self.NL - 1
+        else:
+            raise ValueError("incorrect mechanism.")
+    
+    @property
+    def NLm(self):
+        result = []
+        correction = lambda l: sum((i+2)*l[i] for i in range(len(l))) == 2*self.NJ
+        try:
+            Mmax = self.Mmax
+        except ValueError as e:
+            return str(e)
+        else:
+            for symbols in product(range(self.NL+1), repeat=Mmax-2):
+                NLMmax = self.NL - sum(symbols)
+                answer = symbols+(NLMmax,)
+                if correction(answer) and NLMmax>=0:
+                    result.append(answer)
+            return tuple(result)
 
 class Permutations_show(QWidget, Ui_Form):
     def __init__(self, jointDataFunc, linkDataFunc, dofFunc, parent=None):
@@ -23,13 +71,24 @@ class Permutations_show(QWidget, Ui_Form):
         self.Representation_tree.clear()
         #Show mechanism system.
         root = QTreeWidgetItem(["Mechanism root"])
-        root.setIcon(0, self.windowIcon())
+        root.setIcon(0, QIcon(QPixmap(":/icons/mechanism.png")))
         self.Representation_tree.addTopLevelItem(root)
+        NL = len(tuple(None for vlink in linkData if len(vlink.points)>1))
+        NJ = len(tuple(None for vpoint in jointData if len(vpoint.links)>1))
+        NS = NumberSynthesis(NL, NJ)
+        NS_result = NS.NLm
         root.addChildren([
             QTreeWidgetItem(["Degree of freedom", str(dof)]),
-            QTreeWidgetItem(["Number of links", str(len(tuple(None for vlink in linkData if len(vlink.points)>1)))]),
-            QTreeWidgetItem(["Number of joints", str(len(tuple(None for vpoint in jointData if len(vpoint.links)>1)))]),
+            QTreeWidgetItem(["Number of links", str(NL)]),
+            QTreeWidgetItem(["Number of joints", str(NJ)]),
+            QTreeWidgetItem(["Distant relatives", (
+                NS_result if type(NS_result)==str else
+                ", ".join("; ".join("NL{} = {}".format(i+2, result[i]) for i in range(len(result))) for result in NS_result)
+            )]),
         ])
+        '''
+        TODO: Topologic
+        
         for vlink in linkData:
             node = QTreeWidgetItem([vlink.name])
             node.setIcon(0, QIcon(QPixmap(":/icons/link.png")))
@@ -46,6 +105,7 @@ class Permutations_show(QWidget, Ui_Form):
                 node.addChild(p)
                 self.Representation_tree.expandItem(p)
             self.Representation_tree.expandItem(node)
+        '''
         self.Representation_tree.expandItem(root)
     
     @pyqtSlot(QModelIndex)
