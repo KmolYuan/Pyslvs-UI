7e76dd84a1871b57ea08b906f1cc26a022ac516c
40323230 <daan0014119@gmail.com>
2017-10-26 00:05:00 +0800

    Number synthesis function and UI adjustment.

--- core/synthesis/NumberAndTypeSynthesis/Permutations.py
+++ core/synthesis/NumberAndTypeSynthesis/Permutations.py
@@ -30,6 +30,8 @@ class NumberSynthesis:
     def Mmax(self):
         if self.NL <= self.NJ and self.NJ <= (2*self.NL-3):
             return self.NJ - self.NL + 2
+        elif self.NL == self.NJ and self.NJ == 0:
+            raise ValueError("incorrect mechanism.")
         elif (2*self.NL-3) <= self.NJ and self.NJ <= (self.NL*(self.NL-1)/2):
             return self.NL - 1
         else:
@@ -39,17 +41,15 @@ class NumberSynthesis:
     def NLm(self):
         result = []
         correction = lambda l: sum((i+2)*l[i] for i in range(len(l))) == 2*self.NJ
-        try:
-            Mmax = self.Mmax
-        except ValueError as e:
-            return str(e)
-        else:
-            for symbols in product(range(self.NL+1), repeat=Mmax-2):
-                NLMmax = self.NL - sum(symbols)
-                answer = symbols+(NLMmax,)
-                if correction(answer) and NLMmax>=0:
-                    result.append(answer)
-            return tuple(result)
+        Mmax = self.Mmax
+        for symbols in product(range(self.NL+1), repeat=Mmax-2):
+            NLMmax = self.NL - sum(symbols)
+            if NLMmax < 0:
+                continue
+            answer = symbols+(NLMmax,)
+            if correction(answer):
+                result.append(answer)
+        return tuple(result)
 
 class Permutations_show(QWidget, Ui_Form):
     def __init__(self, jointDataFunc, linkDataFunc, dofFunc, parent=None):
@@ -58,9 +58,8 @@ class Permutations_show(QWidget, Ui_Form):
         self.jointDataFunc = jointDataFunc
         self.linkDataFunc = linkDataFunc
         self.dofFunc = dofFunc
-        self.Representation_tree.expanded.connect(self.resizeHeader)
-        self.Representation_tree.collapsed.connect(self.resizeHeader)
     
+    #Reload button: Auto-combine the mechanism from the workbook.
     @pyqtSlot()
     def on_ReloadMechanism_clicked(self):
         jointData = self.jointDataFunc()
@@ -68,47 +67,24 @@ class Permutations_show(QWidget, Ui_Form):
         dof = self.dofFunc()
         self.Representation_joint.setText("M[{}]".format(", ".join(str(vpoint) for vpoint in jointData)))
         self.Representation_link.setText("M[{}]".format(", ".join(str(vlink) for vlink in linkData)))
-        self.Representation_tree.clear()
-        #Show mechanism system.
-        root = QTreeWidgetItem(["Mechanism root"])
-        root.setIcon(0, QIcon(QPixmap(":/icons/mechanism.png")))
-        self.Representation_tree.addTopLevelItem(root)
         NL = len(tuple(None for vlink in linkData if len(vlink.points)>1))
         NJ = len(tuple(None for vpoint in jointData if len(vpoint.links)>1))
-        NS = NumberSynthesis(NL, NJ)
-        NS_result = NS.NLm
-        root.addChildren([
-            QTreeWidgetItem(["Degree of freedom", str(dof)]),
-            QTreeWidgetItem(["Number of links", str(NL)]),
-            QTreeWidgetItem(["Number of joints", str(NJ)]),
-            QTreeWidgetItem(["Distant relatives", (
-                NS_result if type(NS_result)==str else
-                "; ".join(", ".join("NL{} = {}".format(i+2, result[i]) for i in range(len(result))) for result in NS_result)
-            )]),
-        ])
-        '''
-        TODO: Topologic
-        
-        for vlink in linkData:
-            node = QTreeWidgetItem([vlink.name])
-            node.setIcon(0, QIcon(QPixmap(":/icons/link.png")))
-            root.addChild(node)
-            node.addChild(QTreeWidgetItem(["Number of joints", str(sum(len(jointData[i].links)-1 for i in vlink.points))]))
-            for i in vlink.points:
-                vpoint = jointData[i]
-                p = QTreeWidgetItem(["Point{}".format(i)])
-                p.setIcon(0, QIcon(QPixmap(":/icons/bearing.png")))
-                p.addChild(QTreeWidgetItem(["Joint type", vpoint.typeSTR]))
-                for link in vpoint.links:
-                    if link!=vlink.name:
-                        p.addChild(QTreeWidgetItem(["Connect to Link", link]))
-                node.addChild(p)
-                self.Representation_tree.expandItem(p)
-            self.Representation_tree.expandItem(node)
-        '''
-        self.Representation_tree.expandItem(root)
+        self.NL_input.setValue(NL)
+        self.NJ_input.setValue(NJ)
+        self.DOF_input.setValue(dof)
+        self.on_Combine_number_clicked()
+        #TODO: Topologic
     
-    @pyqtSlot(QModelIndex)
-    def resizeHeader(self, index=None):
-        for i in range(self.Representation_tree.columnCount()):
-            self.Representation_tree.resizeColumnToContents(i)
+    #Show number of links with different number of joints.
+    @pyqtSlot()
+    def on_Combine_number_clicked(self):
+        NS = NumberSynthesis(self.NL_input.value(), self.NJ_input.value())
+        self.Representation_number.clear()
+        try:
+            NS_result = NS.NLm
+        except ValueError as e:
+            self.Representation_number.addItem(str(e))
+        else:
+            for result in NS_result:
+                self.Representation_number.addItem(", ".join("NL{} = {}".format(i+2, result[i]) for i in range(len(result))))
+        self.Representation_number.setCurrentRow(0)
