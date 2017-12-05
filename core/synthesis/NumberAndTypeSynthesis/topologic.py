7493779c5bb027ad20179144de74d3d10f8f6ab7
40323230 <daan0014119@gmail.com>
2017-12-05 21:59:06 +0800

    Topological permutation function.

--- core/synthesis/NumberAndTypeSynthesis/topologic.py
+++ core/synthesis/NumberAndTypeSynthesis/topologic.py
@@ -19,7 +19,7 @@
 
 from anytree import Node, RenderTree
 from anytree.search import findall
-from random import randint, sample
+from itertools import permutations
 from typing import Tuple
 
 def show_tree(node, noname=False):
@@ -31,46 +31,49 @@ def show_tree(node, noname=False):
 
 #Linkage Topological Component
 def make_link(iter: Tuple[int,]):
-    all_link = []
+    link_type = []
     for i, num in enumerate(iter):
         i += 2
         for j in range(num):
-            all_link.append(Node("L{}".format(len(all_link)), limit=i, space=i))
-    link_old = []
-    #Root
-    link_old.append(all_link.pop(randint(0, len(all_link)-1)))
-    #First connect.
-    while all_link:
-        #Let all of links to connect.
-        link = all_link.pop(randint(0, len(all_link)-1))
-        if len(link_old[-1].children)==link_old[-1].limit:
-            link_old.append(link_old[-1].children[0])
-        link.parent = link_old[-1]
-    del all_link
-    for node in link_old:
-        node.space = node.limit - len(node.children)
-    #Decided the remaining connection.
-    get_no_done = lambda: findall(link_old[0], filter_=lambda n: len(n.children)<n.space)
-    while get_no_done():
-        nodes = list(get_no_done())
-        try:
-            l_1, l_2 = sample(nodes, 2)
-        except ValueError:
-            raise ValueError("Loop does not close.")
-        else:
-            l_1.space -= 1
-            l_2.space -= 1
-            Node(l_1.name, limit=l_1.limit, space=-1, parent=l_2)
-            Node(l_2.name, limit=l_2.limit, space=-1, parent=l_1)
-    return link_old[0]
+            link_type.append(i)
+    answer = []
+    for all_link in list(set(permutations(link_type))):
+        all_link = [Node("L{}".format(i), limit=v) for i, v in enumerate(all_link)]
+        links = []
+        #Root
+        links.append(all_link.pop(0))
+        #First connect.
+        while all_link:
+            #Let all of links to connect.
+            link = all_link.pop(0)
+            if (len(links[-1].children) + bool(links[-1].parent))==links[-1].limit:
+                links.append(links[-1].children[0])
+            link.parent = links[-1]
+        #Decided the remaining connection.
+        get_no_done = lambda: findall(links[0], filter_=lambda n: '[' not in n.name and (len(n.children) + bool(links[-1].parent)) < n.limit)
+        error = False
+        while get_no_done():
+            nodes = list(get_no_done())
+            try:
+                l_1, l_2 = nodes[0], nodes[1]
+            except (ValueError, IndexError):
+                error = True
+                break
+            else:
+                Node("[{}]".format(l_1.name), limit=str(l_1.limit), parent=l_2)
+                Node("[{}]".format(l_2.name), limit=str(l_2.limit), parent=l_1)
+        if error:
+            continue
+        if findall(links[0], filter_=lambda n: len([c.name for c in n.children])!=len(set(c.name for c in n.children))):
+            continue
+        answer.append(links[0])
+    return answer
 
 if __name__=='__main__':
     print("Topologic test")
-    root = None
-    while not root:
-        try:
-            root = make_link([5, 4])
-        except ValueError:
-            continue
+    answer = make_link([5, 4])
     #Show tree
-    print(show_tree(root))
+    for root in answer:
+        print(show_tree(root))
+        print('-'*7)
+    print("Answer count: {}".format(len(answer)))
