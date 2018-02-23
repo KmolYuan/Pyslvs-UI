59b47d08958234e6d1d8c596e25b446491690306
40323230 <daan0014119@gmail.com>
2018-02-23 11:20:37 +0800

    Update version.

--- core/info/info.py
+++ core/info/info.py
@@ -29,7 +29,7 @@ except ImportError:
     Qt_Version = "No Qt"
     PyQt_Version = "No PyQt"
 
-VERSION = (18, 2, 0, 'dev')
+VERSION = (18, 2, 0, 'release')
 
 INFO = (
     "Pyslvs {}.{}.{}({})".format(*VERSION),
