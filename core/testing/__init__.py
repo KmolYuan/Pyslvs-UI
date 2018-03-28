7a9108713fc22b47b566579d95d6cdeaee3ded73
KmolYuan <daan0014119@gmail.com>
2018-03-28 14:28:55 +0800

    Add unit test for travis CI.

--- core/testing/__init__.py
+++ core/testing/__init__.py
@@ -6,10 +6,3 @@ __author__ = "Yuan Chang"
 __copyright__ = "Copyright (C) 2016-2018"
 __license__ = "AGPL"
 __email__ = "pyslvs@gmail.com"
-
-import unittest
-
-def UnitTest():
-    unittest.main()
-
-__all__ = ['UnitTest']
