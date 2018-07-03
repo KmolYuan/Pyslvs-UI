176dd2250375ac1209926f7ab0c7bf01f4e8ad71
40323230 <daan0014119@gmail.com>
2018-07-03 15:44:34 +0800

    Module adjustment.

--- core/libs/python_solvespace/__init__.py
+++ core/libs/python_solvespace/__init__.py
@@ -9,10 +9,37 @@ __copyright__ = "Copyright (C) 2016-2018"
 __license__ = "AGPL"
 __email__ = "pyslvs@gmail.com"
 
-from .cad import create2DSystem
-from .planarSolving import slvsProcess
+from .slvs import (
+    #Entities & Constraint
+    Point3d,
+    Workplane,
+    Normal3d,
+    Point2d,
+    LineSegment2d,
+    Constraint,
+    #Result flags
+    SLVS_RESULT_OKAY,
+    SLVS_RESULT_INCONSISTENT,
+    SLVS_RESULT_DIDNT_CONVERGE,
+    SLVS_RESULT_TOO_MANY_UNKNOWNS,
+    #System base
+    System,
+    groupNum,
+    Slvs_MakeQuaternion,
+)
 
 __all__ = [
-    'create2DSystem',
-    'slvsProcess',
+    'Point3d',
+    'Workplane',
+    'Normal3d',
+    'Point2d',
+    'LineSegment2d',
+    'Constraint',
+    'SLVS_RESULT_OKAY',
+    'SLVS_RESULT_INCONSISTENT',
+    'SLVS_RESULT_DIDNT_CONVERGE',
+    'SLVS_RESULT_TOO_MANY_UNKNOWNS',
+    'System',
+    'groupNum',
+    'Slvs_MakeQuaternion',
 ]
