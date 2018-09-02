c9aa8fedc30fc10278d1ed8ed39436b6a4749762
KmolYuan <daan0014119@gmail.com>
2018-09-02 14:04:24 +0800

    Slider simulation corrections for solvespace translator.

--- core/libs/solvespace_translate.py
+++ core/libs/solvespace_translate.py
@@ -61,7 +61,7 @@ def _pos(p: Point2d) -> Tuple[float, float]:
 
 def slvs_solve(
     vpoints: Tuple[VPoint],
-    constraints: Tuple[Tuple[int, int, float]]
+    inputs: Tuple[Tuple[int, int, float]]
 ) -> Tuple[List[Tuple[float, float]], int]:
     """Use element module to convert into solvespace expression."""
     if not vpoints:
@@ -81,39 +81,43 @@ def slvs_solve(
     
     # Limitation of Solvespacce kernel sys.
     point_count = 0
-    slider_count = 0
     for vpoint in vpoints:
-        point_count += 1
-        if vpoint.type in {1, 2}:
-            slider_count += 1
+        if vpoint.type == 0:
+            point_count += 1
+        elif vpoint.type in {1, 2}:
+            point_count += 3
     
-    sys, wp1, h_line = _2d_system(point_count * 2 + slider_count * 2 + len(constraints) * 2)
+    sys, wp1, h_line = _2d_system(point_count * 2 + len(inputs) * 2)
     
     solved_points = []
     slider_points = {}
     slot_points = {}
     for i, vpoint in enumerate(vpoints):
         if vpoint.type == 0:
-            # Has only one coordinate.
+            # Point coordinate.
             x = sys.add_param(vpoint.cx)
             y = sys.add_param(vpoint.cy)
             solved_points.append(Point2d(wp1, x, y))
+            if vpoint.grounded():
+                Constraint.dragged(wp1, solved_points[i])
         elif vpoint.type in {1, 2}:
-            # Has two coordinates.
+            # Base point coordinate.
             bx = sys.add_param(vpoint.c[0][0])
             by = sys.add_param(vpoint.c[0][1])
+            slider_points[i] = Point2d(wp1, bx, by)
+            # Slot coordinate.
             angle = radians(vpoint.angle)
             sx = sys.add_param(vpoint.c[0][0] + cos(angle))
             sy = sys.add_param(vpoint.c[0][1] + sin(angle))
+            slot_points[i] = Point2d(wp1, sx, sy)
+            # Pin coordinate.
             x = sys.add_param(vpoint.c[1][0])
             y = sys.add_param(vpoint.c[1][1])
-            slider_points[i] = Point2d(wp1, bx, by)
-            slot_points[i] = Point2d(wp1, sx, sy)
             solved_points.append(Point2d(wp1, x, y))
-        if vpoint.grounded():
-            Constraint.dragged(wp1, solved_points[i])
-        elif vpoint.pin_grounded():
-            Constraint.dragged(wp1, slider_points[i])
+            if vpoint.grounded():
+                Constraint.dragged(wp1, slider_points[i])
+            elif vpoint.pin_grounded():
+                Constraint.dragged(wp1, solved_points[i])
     
     # Link constraints.
     for vlink in vlinks:
@@ -151,17 +155,18 @@ def slvs_solve(
                     Constraint.on(wp1, p1, p2)
     
     # Slider constraints.
-    for i, pb in slider_points.items():
+    for i in slider_points:
         vpoint = vpoints[i]
-        slider_slot = LineSegment2d(wp1, pb, slot_points[i])
+        slider_slot = LineSegment2d(wp1, slider_points[i], slot_points[i])
         if vpoint.grounded():
             # Slot is grounded.
             if vpoint.angle in {0., 180.}:
                 Constraint.parallel(wp1, h_line, slider_slot)
             else:
-                Constraint.angle(wp1, radians(vpoint.angle), h_line, slider_slot)
+                Constraint.angle(wp1, vpoint.angle, h_line, slider_slot)
+            Constraint.on(wp1, solved_points[i], slider_slot)
             if vpoint.has_offset():
-                Constraint.distance(vpoint.offset(), wp1, pb, solved_points[i])
+                Constraint.distance(vpoint.offset(), wp1, slider_points[i], solved_points[i])
         else:
             # Slider between links.
             for vlink in vpoint.links[:1]:
@@ -172,21 +177,22 @@ def slvs_solve(
                         continue
                     f1 = vlinks[vlink][1]
                 if vpoints[f1].is_slot_link(vlink):
-                    helper = LineSegment2d(wp1, pb, slider_points[f1])
+                    helper = LineSegment2d(wp1, slider_points[i], slider_points[f1])
                 else:
-                    helper = LineSegment2d(wp1, pb, solved_points[f1])
+                    helper = LineSegment2d(wp1, slider_points[i], solved_points[f1])
                 angle = vpoint.slope_angle(vpoints[f1]) - vpoint.angle
                 if angle in {0., 180.}:
                     Constraint.parallel(wp1, slider_slot, helper)
                 else:
-                    Constraint.angle(wp1, radians(angle), slider_slot, helper)
+                    Constraint.angle(wp1, angle, slider_slot, helper)
+                Constraint.on(wp1, solved_points[i], slider_slot)
                 if vpoint.has_offset():
-                    Constraint.distance(vpoint.offset(), wp1, pb, solved_points[i])
+                    Constraint.distance(vpoint.offset(), wp1, slider_points[i], solved_points[i])
         # P joint.
         if vpoint.type == 1:
             for vlink in vpoint.links[1:]:
                 f1 = vlinks[vlink][0]
-                if f1 == a:
+                if f1 == i:
                     if len(vlinks[vlink]) < 2:
                         # If no any friend.
                         continue
@@ -199,9 +205,9 @@ def slvs_solve(
                 if angle in {0., 180.}:
                     Constraint.parallel(wp1, slider_slot, helper)
                 else:
-                    Constraint.angle(wp1, radians(angle), slider_slot, helper)
+                    Constraint.angle(wp1, angle, slider_slot, helper)
     
-    for p0, p1, angle in constraints:
+    for p0, p1, angle in inputs:
         """The constraints of drive shaft.
         
         Simulate the input variables to the mechanism.
@@ -212,8 +218,9 @@ def slvs_solve(
         else:
             p_base = slider_points[p0]
         
-        x = sys.add_param(round(vpoints[p0].cx + 10 * cos(radians(angle)), 8))
-        y = sys.add_param(round(vpoints[p0].cy + 10 * sin(radians(angle)), 8))
+        angle = radians(angle)
+        x = sys.add_param(vpoints[p0].cx + cos(angle))
+        y = sys.add_param(vpoints[p0].cy + sin(angle))
         p_hand = Point2d(wp1, x, y)
         Constraint.dragged(wp1, p_hand)
         # The virtual link that dragged by "hand".
@@ -224,7 +231,7 @@ def slvs_solve(
         else:
             p_drive = slider_points[p1]
         link = LineSegment2d(wp1, p_base, p_drive)
-        Constraint.angle(wp1, .5, link, leader)
+        Constraint.angle(wp1, 0.5, link, leader)
     
     # Solve
     result_flag = sys.solve()
