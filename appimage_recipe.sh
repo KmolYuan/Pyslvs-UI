9368cb02e2bc027f387ef58d834742a5d91ad927
KmolYuan <pyslvs@gmail.com>
2019-09-03 00:02:22 +0800

    Reformat shell script.

--- appimage_recipe.sh
+++ appimage_recipe.sh
@@ -11,8 +11,8 @@
 APP=pyslvs
 LOWERAPP=${APP,,}
 
-mkdir -p ENV/${APP}.AppDir/
-cd ENV/${APP}.AppDir/
+mkdir -p ENV/${APP}.AppDir
+cd ENV/${APP}.AppDir
 
 ########################################################################
 # Create a virtualenv inside the AppDir
@@ -22,21 +22,18 @@ MY_APPDIR=${PWD}
 
 mkdir -p usr
 virtualenv ./usr --python=python3 --always-copy --verbose
-mkdir -p usr/bin/
+mkdir -p usr/bin
 source usr/bin/activate
 
-
-# Source some helper functions.
+# Source some helper functions
 wget -q https://raw.githubusercontent.com/AppImage/AppImages/master/functions.sh -O ./functions.sh
 . ./functions.sh
 
-
-# Show python and pip versions.
+# Show python and pip versions
 python --version
 pip --version
 
-
-# Install python dependencies into the virtualenv.
+# Install python dependencies into the virtualenv
 pip install -r ../../requirements.txt
 cd ../../depend/pyslvs
 python setup.py install
@@ -45,8 +42,7 @@ cd ../../depend/solvespace/cython
 python setup.py install
 cd ${MY_APPDIR}
 
-
-# Copy all built-in scripts.
+# Copy all built-in scripts
 PYVER=$(python -c "from distutils import sysconfig;print(sysconfig.get_config_var('VERSION'))")
 PYDIR=$(python -c "from distutils import sysconfig;print(sysconfig.get_config_var('DESTLIB'))")
 MY_PYDIR=${MY_APPDIR}/usr/lib/python${PYVER}
@@ -57,46 +53,45 @@ rm -fr -v ${MY_PYDIR}/distutils
 echo "Copy built-in script patch from '${PYDIR}' to '${MY_PYDIR}' ..."
 cd ${PYDIR}
 for f in *; do
-    if [[ ${f} == "__pycache__" ]] || [[ ${f} == "test" ]] \
-        || [[ ${f} == "venv" ]] || [[ ${f} == "idlelib" ]]; then continue; fi
+  if [[ ${f} == "__pycache__" ]] || [[ ${f} == "test" ]] ||
+    [[ ${f} == "venv" ]] || [[ ${f} == "idlelib" ]]; then continue; fi
 
-    if [[ ${f} == *.py ]]; then
-        cp -n -v ${f} ${MY_PYDIR}
+  if [[ ${f} == *.py ]]; then
+    cp -n -v ${f} ${MY_PYDIR}
+  fi
+
+  if [[ ! -d ${f} ]]; then continue; fi
+
+  echo "Create '${MY_PYDIR}/${f}'"
+  mkdir -p ${MY_PYDIR}/${f}
+  cp -n -v ${f}/*.py ${MY_PYDIR}/${f}
+  cp -n -v ${f}/*.so ${MY_PYDIR}/${f}
+
+  cd ${f}
+
+  for sub_f in *; do
+    if [[ ${sub_f} == "__pycache__" ]]; then continue; fi
+
+    if [[ ${sub_f} == *.py ]]; then
+      cp -n -v ${sub_f} ${MY_PYDIR}/${f}
     fi
 
-    if [[ ! -d ${f} ]]; then continue; fi
+    if [[ ! -d ${sub_f} ]]; then continue; fi
 
-    echo "Create '${MY_PYDIR}/${f}'"
-    mkdir -p ${MY_PYDIR}/${f}
-    cp -n -v ${f}/*.py ${MY_PYDIR}/${f}
-    cp -n -v ${f}/*.so ${MY_PYDIR}/${f}
+    echo "Create '${MY_PYDIR}/${f}/${sub_f}'"
+    mkdir -p ${MY_PYDIR}/${f}/${sub_f}
+    cp -n -v ${sub_f}/*.py ${MY_PYDIR}/${f}/${sub_f}
+    cp -n -v ${sub_f}/*.so ${MY_PYDIR}/${f}/${sub_f}
+  done
 
-    cd ${f}
-
-    for sub_f in *; do
-        if [[ ${sub_f} == "__pycache__" ]]; then continue; fi
-
-        if [[ ${sub_f} == *.py ]]; then
-            cp -n -v ${sub_f} ${MY_PYDIR}/${f}
-        fi
-
-        if [[ ! -d ${sub_f} ]]; then continue; fi
-
-        echo "Create '${MY_PYDIR}/${f}/${sub_f}'"
-        mkdir -p ${MY_PYDIR}/${f}/${sub_f}
-        cp -n -v ${sub_f}/*.py ${MY_PYDIR}/${f}/${sub_f}
-        cp -n -v ${sub_f}/*.so ${MY_PYDIR}/${f}/${sub_f}
-    done
-
-    cd ..
+  cd ..
 done
 cd ${MY_APPDIR}
-
 
 # Python libraries
 SCRIPTDIR=$(python -c "from distutils import sysconfig;print(sysconfig.get_config_var('SCRIPTDIR'))")
 for f in ${SCRIPTDIR}/libpython3*.so*; do
-    cp -n -v ${f} usr/lib
+  cp -n -v ${f} usr/lib
 done
 
 deactivate
@@ -129,7 +124,7 @@ VERSION=$(python3 -c "from pyslvs import __version__; print(__version__)")
 echo "${VERSION}"
 cd ENV/${APP}.AppDir
 
-cat > ${LOWERAPP}.desktop <<EOF
+cat >${LOWERAPP}.desktop <<EOF
 [Desktop Entry]
 Name=${APP}
 Exec=${LOWERAPP}
@@ -148,7 +143,9 @@ cp ../../icons/main.png ${LOWERAPP}.png
 # Bundle dependencies
 ########################################################################
 
-copy_deps ; copy_deps ; copy_deps
+copy_deps
+copy_deps
+copy_deps
 delete_blacklisted
 move_lib
 
