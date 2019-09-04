3b59851894b5989bb3e3c562fdd1289b39ffad6a
KmolYuan <pyslvs@gmail.com>
2019-09-04 10:24:56 +0800

    Simplify shell script and CI option.

--- appimage_recipe.sh
+++ appimage_recipe.sh
@@ -11,88 +11,58 @@
 APP=pyslvs
 LOWERAPP=${APP,,}
 
-mkdir -p ENV/${APP}.AppDir
-cd ENV/${APP}.AppDir
+BASEDIR=$(dirname "$(readlink -f "${0}")")
+cd ${BASEDIR} || exit
+ENV=${BASEDIR}/ENV
+APPDIR=${ENV}/${APP}.AppDir
+
+mkdir -p ${APPDIR}
+
+# Source some helper functions
+wget -q https://raw.githubusercontent.com/AppImage/pkg2appimage/master/functions.sh -O ${ENV}/functions.sh
+# shellcheck disable=SC1090
+. ${ENV}/functions.sh
 
 ########################################################################
 # Create a virtualenv inside the AppDir
 ########################################################################
 
-MY_APPDIR=${PWD}
-
-mkdir -p usr
+cd ${APPDIR} && mkdir -p usr/bin
 virtualenv ./usr --python=python3 --always-copy --verbose
-mkdir -p usr/bin
 source usr/bin/activate
-
-# Source some helper functions
-wget -q https://raw.githubusercontent.com/AppImage/AppImages/master/functions.sh -O ./functions.sh
-. ./functions.sh
 
 # Show python and pip versions
 python --version
 pip --version
 
 # Install python dependencies into the virtualenv
-pip install -r ../../requirements.txt
-cd ../../depend/pyslvs
-python setup.py install
-cd ${MY_APPDIR}
-cd ../../depend/solvespace/cython
-python setup.py install
-cd ${MY_APPDIR}
+pip install -r ${BASEDIR}/requirements.txt
+cd ${BASEDIR}/depend/pyslvs && python setup.py install
+cd ${BASEDIR}/depend/solvespace/cython && python setup.py install
+cd ${APPDIR} || exit
 
 # Copy all built-in scripts
 PYVER=$(python -c "from distutils import sysconfig;print(sysconfig.get_config_var('VERSION'))")
 PYDIR=$(python -c "from distutils import sysconfig;print(sysconfig.get_config_var('DESTLIB'))")
-MY_PYDIR=${MY_APPDIR}/usr/lib/python${PYVER}
+MY_PYDIR=${APPDIR}/usr/lib/python${PYVER}
 
 echo "Remove venv distutils ..."
-rm -fr -v ${MY_PYDIR}/distutils
+rm -fr ${MY_PYDIR}/distutils
 
-echo "Copy built-in script patch from '${PYDIR}' to '${MY_PYDIR}' ..."
-cd ${PYDIR}
-for f in *; do
-  if [[ ${f} == "__pycache__" ]] || [[ ${f} == "test" ]] ||
-    [[ ${f} == "venv" ]] || [[ ${f} == "idlelib" ]]; then continue; fi
-
-  if [[ ${f} == *.py ]]; then
-    cp -n -v ${f} ${MY_PYDIR}
-  fi
-
-  if [[ ! -d ${f} ]]; then continue; fi
-
-  echo "Create '${MY_PYDIR}/${f}'"
-  mkdir -p ${MY_PYDIR}/${f}
-  cp -n -v ${f}/*.py ${MY_PYDIR}/${f}
-  cp -n -v ${f}/*.so ${MY_PYDIR}/${f}
-
-  cd ${f}
-
-  for sub_f in *; do
-    if [[ ${sub_f} == "__pycache__" ]]; then continue; fi
-
-    if [[ ${sub_f} == *.py ]]; then
-      cp -n -v ${sub_f} ${MY_PYDIR}/${f}
-    fi
-
-    if [[ ! -d ${sub_f} ]]; then continue; fi
-
-    echo "Create '${MY_PYDIR}/${f}/${sub_f}'"
-    mkdir -p ${MY_PYDIR}/${f}/${sub_f}
-    cp -n -v ${sub_f}/*.py ${MY_PYDIR}/${f}/${sub_f}
-    cp -n -v ${sub_f}/*.so ${MY_PYDIR}/${f}/${sub_f}
-  done
-
-  cd ..
+echo "Copy builtin scripts from '${PYDIR}' to '${MY_PYDIR}' ..."
+cd ${PYDIR} || exit
+for p in "*.py" "*.so"; do
+  find . -name "${p}" -exec install -v -D {} ${MY_PYDIR}/{} \;
 done
-cd ${MY_APPDIR}
+
+cd ${MY_PYDIR} || exit
+for d in "test" "venv" "idlelib"; do
+  rm -fr ${d}
+done
 
 # Python libraries
-SCRIPTDIR=$(python -c "from distutils import sysconfig;print(sysconfig.get_config_var('SCRIPTDIR'))")
-for f in ${SCRIPTDIR}/libpython3*.so*; do
-  cp -n -v ${f} usr/lib
-done
+cd ${APPDIR} || exit
+cp -n -v "$(python -c "from distutils import sysconfig;print(sysconfig.get_config_var('SCRIPTDIR'))")"/libpython3*.so* usr/lib
 
 deactivate
 
@@ -101,28 +71,26 @@ deactivate
 ########################################################################
 
 # Make launch script
-cp ../../launch_pyslvs.py usr/bin/${LOWERAPP}
+cp ${BASEDIR}/launch_pyslvs.py usr/bin/${LOWERAPP}
 sed -i "1i\#!/usr/bin/env python3" usr/bin/${LOWERAPP}
 chmod a+x usr/bin/${LOWERAPP}
 
-cp ../../icons_rc.py usr/bin
-cp ../../preview_rc.py usr/bin
-cd ../../core
-find . -name "*.py" -exec install -D {} ${MY_APPDIR}/usr/bin/core/{} \;
-cd ${MY_APPDIR}/usr/bin
-
-cd ../..
+cp ${BASEDIR}/icons_rc.py usr/bin
+cp ${BASEDIR}/preview_rc.py usr/bin
+cd ${BASEDIR}/core || exit
+find . -name "*.py" -exec install -v -D {} ${APPDIR}/usr/bin/core/{} \;
 
 ########################################################################
 # Finalize the AppDir
 ########################################################################
 
+cd ${APPDIR} || exit
 get_apprun
 
-cd ../..
+cd ${BASEDIR} || exit
 VERSION=$(python3 -c "from pyslvs import __version__; print(__version__)")
 echo "${VERSION}"
-cd ENV/${APP}.AppDir
+cd ${APPDIR} || exit
 
 cat >${LOWERAPP}.desktop <<EOF
 [Desktop Entry]
@@ -137,7 +105,7 @@ EOF
 
 # Make the AppImage ask to "install" itself into the menu
 get_desktopintegration ${LOWERAPP}
-cp ../../icons/main.png ${LOWERAPP}.png
+cp ${BASEDIR}/icons/main.png ${LOWERAPP}.png
 
 ########################################################################
 # Bundle dependencies
@@ -149,12 +117,11 @@ copy_deps
 delete_blacklisted
 move_lib
 
-rm -fr ./opt
-rm -fr ./usr/share
+rm -fr ./opt ./usr/share
 
 ########################################################################
 # Package the AppDir as an AppImage
 ########################################################################
 
-cd ..
+cd ${ENV} || exit
 generate_type2_appimage
