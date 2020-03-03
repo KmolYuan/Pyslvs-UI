2b2bebc1b00464e02e90cdd66bd00150339844fd
KmolYuan <pyslvs@gmail.com>
2020-03-03 23:27:09 +0800

    Depress Qt warning.

--- pyslvs_ui/__main__.py
+++ pyslvs_ui/__main__.py
@@ -20,9 +20,6 @@ def main() -> None:
     """Startup function."""
     global _app
     t0 = process_time()
-    from qtpy.QtCore import Qt, QDir, QLockFile
-    from qtpy.QtWidgets import QApplication, QSplashScreen
-    from qtpy.QtGui import QPixmap
     from .info import ARGUMENTS, logger
     if ARGUMENTS.cmd == 'test':
         from importlib import import_module
@@ -32,7 +29,13 @@ def main() -> None:
         shutdown()
         exit(0)
     elif ARGUMENTS.cmd in {'gui', None}:
+        from qtpy.QtCore import Qt, QDir, QLockFile, qInstallMessageHandler
+        from qtpy.QtWidgets import QApplication, QSplashScreen
+        from qtpy.QtGui import QPixmap
         _app = QApplication(argv)
+        # Depress Qt warning
+        qInstallMessageHandler(lambda _0, _1, _2: None)
+        # One instance
         lf = QLockFile(join(QDir.tempPath(), "pyslvs.lock"))
         if not lf.tryLock(100):
             logger.info("Pyslvs can only start one instance.")
@@ -50,14 +53,20 @@ def main() -> None:
             _app.setStyle('fusion')
         # Main window
         from .main_window import MainWindow
-        sp.finish(MainWindow.new())
+        w = MainWindow.new()
+        sp.finish(w)
         sp.deleteLater()
-        del sp
         logger.info(f"Startup with: {process_time() - t0:.02f}s")
+        if not ARGUMENTS.debug_mode:
+            w.console_connect()
+        del sp, w
         qt_exit_code = _app.exec_()
         del lf
         shutdown()
         exit(qt_exit_code)
+    else:
+        # Command line mode
+        logger.info(f"Start CLI: {process_time() - t0:.02f}s")
 
 
 if __name__ == '__main__':
